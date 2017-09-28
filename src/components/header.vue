fc97d249915b448fd8d8ea86be1f606cadc8a063
hido <924838091@qq.com>
2017-09-28 12:53:08 +0800

    吊袜带

---
+++ src/components/header.vue
@@ -0,0 +1,19 @@
+<style>
+  #header{
+    height:50px;
+    box-shadow: 0 2px 4px 0 rgba(0, 0, 0, 0.15);
+    text-align: center;
+  }
+</style>
+
+<template>
+  <div id="header">
+    <h2>我是头部组件</h2>
+  </div>
+</template>
+
+<script>
+    export default{
+        name:'header'
+    }
+</script>
